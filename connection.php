15870b65e6be281503c0249c5e91a65c9506922d
guraansh <52524687+guraansh@users.noreply.github.com>
2022-04-13 11:16:01 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php  
+     	$servername = "localhost";
+	$username = "root";  
+       	$password = "";  
+       	$conn = mysqli_connect ($servername , $username , $password,'travel') or die("unable to connect to host");  
+	/*$sql = mysqli_select_db ('travel',$conn) or die("unable to connect to database");*/	
+?>
